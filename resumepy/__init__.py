2476fef95f006b5d85fd655c970ba35b04ed999a
Christopher C Strelioff <chris.strelioff@gmail.com>
2014-10-02 12:10:35 -0700

    add method imports to __ini__.py

--- resumepy/__init__.py
+++ resumepy/__init__.py
@@ -1,1 +1,10 @@
+from .process import create_parser
+from .process import process_html
+from .process import process_pdf
+from .process import process_text
+
+from .utils import check_dir
+from .utils import copy_file
+from .utils import mkdirs
+from .utils import make_temp_directory
 
