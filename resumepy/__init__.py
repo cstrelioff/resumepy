851090192c07c87249c6d0f06c3e603e32504e20
Christopher C Strelioff <chris.strelioff@gmail.com>
2014-10-02 16:35:23 -0700

    modify __init__.py to satisfy testing, importing and pep

--- resumepy/__init__.py
+++ resumepy/__init__.py
@@ -1,10 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 Christopher C. Strelioff <chris.strelioff@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""__init__.py
+
+The resumepy package.
+
+"""
+
 from .process import create_parser
 from .process import process_html
 from .process import process_pdf
 from .process import process_text
 
-from .utils import check_dir
-from .utils import copy_file
-from .utils import mkdirs
-from .utils import make_temp_directory
+__all__ = ['create_parser', 'process_html', 'process_pdf', 'process_text']
 
